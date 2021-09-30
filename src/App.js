35658a05ea75d155733dec035f4a931523d0f21f
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2021-09-30 13:06:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const app = new Realm.App(APP_ID);
 // Configure the ApolloClient to connect to your app's GraphQL endpoint
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: `https://realm.mongodb.com/api/client/v2.0/app/myiot-sxwcb/graphql`,
+    uri: `https://realm.mongodb.com/api/client/v2.0/app/` + APP_ID + `/graphql`,
     // We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
     // The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
     // access token before sending the request.
